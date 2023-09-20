70f0eee5d7e5b1f949ca2695b56143a3928be34e
rishabhi-2022 <thank.ub44>
2023-09-20 06:41:27 +0530

    Inital Commit

---
+++ Java-Project/src/main/java/com/nagarro/Java/Project/JavaProjectApplication.java
@@ -0,0 +1,19 @@
+package com.nagarro.Java.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+	}
+	@GetMapping("/")
+	
+	String hello() {
+		return "Hello, World !";
+	}
+
+}
